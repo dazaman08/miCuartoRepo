4d88abab6312574a6fcb270ede713bf0ece4348a
Sergio Daza <davidaza2002@gmail.com>
2025-07-23 16:38:56 -0500

    Metodo de platzi agregado

--- API/app.py
+++ API/app.py
@@ -10,7 +10,13 @@ def get_familia():
     rows = ["Amin", "Marce", "Miranda"]
     return rows
 
-@app.get("/superheroesDC")
+
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
+    return rows
+
+
+@app.get("/cursoPlatzi")
+def get_curso_platzi():
+    rows = ["Curso de Python", "Curso de JavaScript", "Curso de React", "Curso de Node.js"]
     return rows
